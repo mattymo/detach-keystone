d0c348c35655de405d9a1a505edea7d694b43c33
Bartłomiej Piotrowski <bpiotrowski@mirantis.com>
2015-07-22 14:40:55 +0200

    Refactor for role as plugin and vip from plugin

--- post_install.sh
+++ post_install.sh
@@ -1,33 +1,3 @@
 #!/bin/bash
-PLUGIN_DIR=/var/www/nailgun/plugins/%{name}
-declare -a roles=(`ls ${PLUGIN_DIR}/deployment_scripts/roles/`)
 
-FUEL='/usr/bin/fuel'
-REL=`$FUEL rel | grep -i ubuntu | awk '{print $1}' | head`
-FUEL_REL=`$FUEL rel | grep -i ubuntu | awk '{print $NF}'`
-
-function create_vip {
-  fuel rel --rel $REL --network download
-  if ! grep -q -- '    - database' "release_${REL}/networks.yaml"; then
-    sed -i 's/    - vrouter/    - vrouter\n    - keystone/' \
-      "release_${REL}/networks.yaml"
-    fuel rel --rel $REL --network --upload
-  fi
-}
-
-#FIXME(bpiotrowski): remove when role-as-a-plugin is merged
-function create_roles {
-  for role in ${roles[@]}; do
-    $FUEL role --rel $REL | awk '{print $1}' | grep -qx ${role%.*}
-      if [[ $? -eq 0 ]]; then
-        $FUEL role --rel $REL --update --file ${PLUGIN_DIR}/deployment_scripts/roles/${role}
-      else
-        $FUEL role --rel $REL --create --file ${PLUGIN_DIR}/deployment_scripts/roles/${role}
-      fi
-  done
-}
-
-create_roles
-create_vip
-cp -a ${PLUGIN_DIR}/deployment_scripts/detach-keystone /etc/puppet/$FUEL_REL/modules/osnailyfacter/modular/
-$FUEL rel --sync-deployment-tasks --dir /etc/puppet/$FUEL_REL
+exit 0
