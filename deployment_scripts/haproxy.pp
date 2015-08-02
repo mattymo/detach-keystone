49b9be704c2b945891004359d1dce6795cb66730
Matthew Mosesohn <mmosesohn@mirantis.com>
2015-08-02 12:55:25 +0300

    fix haproxy

--- deployment_scripts/haproxy.pp
+++ deployment_scripts/haproxy.pp
@@ -1,21 +1,47 @@
 notice('MODULAR: detach-keystone/haproxy.pp')
 
-$keystone_ipaddresses  = hiera('keystone_ipaddresses')
-$keystone_names        = hiera('keystone_names')
-$keystone_vip          = hiera('keystone_vip')
+$network_metadata = hiera_hash('network_metadata')
+$keystone_hash    = hiera_hash('keystone', {})
+# enabled by default
+$use_keystone = pick($keystone_hash['enabled'], true)
+$public_ssl_hash = hiera('public_ssl')
+
+#todo(sv): change to 'keystone' as soon as keystone as node-role was ready    
+$keystones_address_map = get_node_to_ipaddr_map_by_network_role(get_nodes_hash_by_roles($network_metadata, ['primary-standalone-keystone', 'standalone-keystone']), 'keystone/api')
+
+if ($use_keystone) {
+  $server_names        = pick(hiera_array('keystone_names', undef),
+                              keys($keystones_address_map))
+  $ipaddresses         = pick(hiera_array('keystone_ipaddresses', undef),
+                              values($keystones_address_map))
+  $public_virtual_ip   = pick(hiera('public_service_endpoint', undef), hiera('public_vip'))
+  $internal_virtual_ip = pick(hiera('service_endpoint', undef), hiera('management_vip'))
+
+
+  # configure keystone ha proxy
+  class { '::openstack::ha::keystone':
+    internal_virtual_ip => $internal_virtual_ip,
+    ipaddresses         => $ipaddresses,
+    public_virtual_ip   => $public_virtual_ip,
+    server_names        => $server_names,
+    public_ssl          => $public_ssl_hash['services'],
+  }
+
+  Package['socat'] -> Class['openstack::ha::keystone']
+
+  package { 'socat':
+    ensure => 'present',
+  }
+}
 
 Haproxy::Service        { use_include => true }
 Haproxy::Balancermember { use_include => true }
 
-Package['socat'] -> Class['openstack::ha::keystone']
 
-class { 'openstack::ha::keystone':
-  server_names        => $keystone_names,
-  ipaddresses         => $keystone_ipaddresses,
-  public_virtual_ip   => $keystone_vip,
-  internal_virtual_ip => $keystone_vip,
+#FIXME(mattymo): base haproxy stats task doesn't support specifying VIP
+class { '::openstack::ha::stats':
+  internal_virtual_ip => $internal_virtual_ip,
+  public_virtual_ip   => $internal_virtual_ip,
 }
-package { 'socat':
-  ensure => 'present',
-}
+
 
